f4d585991316e7b7e7146dc90beda5a60edf9967
Tampoka <goal.thanks@gmail.com>
2021-11-01 23:55:34 +0900

    adding newPost to posts

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
            title,
            body
        }
-        console.log(newPost)
+        setPosts([...posts,newPost])
     }
     return (
         <div className="App">
