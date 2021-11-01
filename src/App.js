b5c5fb5518bce980536d1204852d96d6497cdd1a
Tampoka <goal.thanks@gmail.com>
2021-11-01 14:28:53 +0900

    passing post data in props

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ function App() {
 
     return (
         <div className="App">
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
+            <PostItem post={{id:1, title:"Higher Order Functions in javascript.",description:"Functions that operate on other " +
+                    "functions, either by taking them as arguments or by returning them, are called higher-order functions. Higher order " +
+                    "functions are a result of functions being first-class citizens in javascript."}}/>
         </div>
     );
 }
