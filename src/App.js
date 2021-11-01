3dd8e5184654bfc72c6f0ef7e2bbdb0dc8456fd0
Tampoka <goal.thanks@gmail.com>
2021-11-01 14:19:11 +0900

    made Post component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import {useState} from "react";
-import  "./styles/App.css"
+import "./styles/App.css"
+import PostItem from "./components/PostItem";
+
 function App() {
     const [value, setValue] = useState('text inside input')
 
     return (
         <div className="App">
-            <div className="post">
-                <div className="post__content">
-                    <strong>1. Higher Order Functions in javascript.</strong>
-                    <div>Functions that operate on other functions, either by taking them as arguments or by returning them, are called higher-order functions.
-                        Higher order functions are a result of functions being first-class citizens in javascript.</div>
-                </div>
-            <div className="post__btns">
-                <button>Delete</button>
-            </div>
-            </div>
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
         </div>
     );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const PostItem = () => {
+    return (
+        <div>
+            <div className="post">
+                <div className="post__content">
+                    <strong>1. Higher Order Functions in javascript.</strong>
+                    <div>Functions that operate on other functions, either by taking them as arguments or by returning them, are called higher-order functions.
+                        Higher order functions are a result of functions being first-class citizens in javascript.</div>
+                </div>
+                <div className="post__btns">
+                    <button>Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PostItem;
