a096a59164b71ba26cd5939e55d747a98b086fbf
Tampoka <goal.thanks@gmail.com>
2021-11-02 21:05:59 +0900

    useEffect for fetching Posts on load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import "./styles/App.css"
 import PostsList from "./components/PostsList";
 import PostForm from "./components/PostForm";
@@ -17,6 +17,10 @@ function App() {
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts=usePosts(posts,filter.sort, filter.query)
 
+    useEffect(()=>{
+        fetchPosts()
+    },[filter])
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
@@ -33,7 +37,7 @@ function App() {
 
     return (
         <div className="App">
-            <button onClick={fetchPosts}>GET POSTS</button>
+            {/*<button onClick={fetchPosts}>GET POSTS</button>*/}
             <MyButton style={{marginTop: 50}} onClick={() => setModal(true)}>
                 Add post
             </MyButton>
