c44ea7fd6423329555fd0a0e3ccf18438a13d1db
Tampoka <goal.thanks@gmail.com>
2021-11-02 17:48:04 +0900

    put PostForm into modal

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostsList from "./components/PostsList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
     const [posts, setPosts] = useState([{id: 1, title: "Higher Order Functions in javascript.", body: "description"},
@@ -11,6 +12,7 @@ function App() {
         {id: 3, title: "What is Currying in javascript?", body: "dfsffs"}])
 
     const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false)
 
     const sortedPosts = useMemo(() => {
             console.log('finished')
@@ -23,6 +25,7 @@ function App() {
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     const sortedAndSearchedPosts = useMemo(() => {
@@ -35,10 +38,12 @@ function App() {
 
     return (
         <div className="App">
-            <MyModal>
-
+            <MyButton style={{marginTop: 50}} onClick={() => setModal(true)}>
+                Add user
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
             </MyModal>
-            <PostForm create={createPost}/>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter
                 setFilter={setFilter}
--- src/components/UI/modal/MyModal.jsx
+++ src/components/UI/modal/MyModal.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import s from './MyModal.module.css'
 
-const MyModal = ({children,visible, setVisible}) => {
-    const rootClasses=[s.myModal]
-    if(visible){
+const MyModal = ({children, visible, setVisible}) => {
+    const rootClasses = [s.myModal]
+    if (visible) {
         rootClasses.push(s.active)
     }
     return (
-        <div className={rootClasses.join(' ')}>
+        <div className={rootClasses.join(' ')}
+             onClick={() => setVisible(false)}>
             <div className={s.myModalContent}>
                 {children}
             </div>
