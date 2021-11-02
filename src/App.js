02d646e44531af5a2a024eb5202d63a4a6ff369d
Tampoka <goal.thanks@gmail.com>
2021-11-02 10:34:43 +0900

    using setPost for creating Post object

--- src/App.js
+++ src/App.js
@@ -9,31 +9,24 @@ function App() {
         {id: 2, title: "What is an Immediately Invoked Function in javascript?", body: "description"},
         {id: 3, title: "What is Currying in javascript?"}])
 
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
+   const [post,setPost]=useState({title:'',body:''})
 
     const addNewPost = (e) => {
         e.preventDefault()
-       const newPost={
-            id:Date.now(),
-           title,
-           body
-       }
-        setPosts([...posts,newPost])
-        setTitle('')
-        setBody('')
+        setPosts([...posts, {...post, id:Date.now()}])
+       setPost({title:'',body:''})
     }
     return (
         <div className="App">
             <form>
                 <MyInput
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
+                    value={post.title}
+                    onChange={(e) => setPost({...post,title:e.target.value})}
                     type="text"
                     placeholder="Post title"/>
                 <MyInput
-                    value={body}
-                    onChange={(e) => setBody(e.target.value)}
+                    value={post.body}
+                    onChange={(e) => setPost({...post, body:e.target.value})}
                     type=" text"
                     placeholder="Post description"/>
                 <MyButton onClick={addNewPost}>Add post</MyButton>
