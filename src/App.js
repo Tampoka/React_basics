456db47028a411f59f0a3b6d878125804ae02c5b
Tampoka <goal.thanks@gmail.com>
2021-11-05 13:18:11 +0900

    Add state for posts total count

--- src/App.js
+++ src/App.js
@@ -14,11 +14,12 @@ function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
+    const [totalCount, setTotalcount]=useState(0)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
     const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const posts = await PostService.getAll()
-        setPosts(posts)
+        const response = await PostService.getAll()
+        setPosts(response.data)
     })
 
     useEffect(() => {
