97740f35afa0f2f4dd75567b7901a4071dd5d564
Tampoka <goal.thanks@gmail.com>
2021-11-20 21:47:58 +0900

    Create PostIdPage

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import {useFetching} from "../hooks/useFetching";
+import PostService from "../API/PostService";
+import Loader from "../components/UI/loader/Loader";
 
 const PostIdPage = () => {
+    const params = useParams()
+    const [post, setPost] = useState({})
+
+    const [fetchPostsById, isLoading, error] = useFetching(async (id) => {
+        const response = await PostService.getbyId(params.id)
+        setPost(response.data)
+    })
+
+    useEffect(() => {
+        fetchPostsById(params.id)
+    },[])
     return (
         <div>
-            <h1>This is post page</h1>
+            <h1>This is post page with id={params.id}</h1>
+            {isLoading
+                ? <Loader/>
+                : <div>{post.id}. {post
+                }</div>
+            }
         </div>
     );
 };
