a756072625c8b95cef404def4ca79f18c145338f
Tampoka <goal.thanks@gmail.com>
2021-11-20 16:51:48 +0900

    Add Error page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import About from "./pages/About";
 import "./styles/App.css"
 import Navbar from "./components/UI/Navbar/Navbar";
+import Error from "./pages/Error";
 
 const App = () => {
     return (
@@ -12,7 +13,8 @@ const App = () => {
             <Routes>
                 <Route path="/about" element={<About/>}/>
                 <Route path="/posts" element={<Posts/>}/>
-                <Route path="*" element={<Navigate to="/posts"/>}/>
+                <Route path="/error" element={<Error/>}/>
+                <Route path="*" element={<Navigate to="/error"/>}/>
             </Routes>
         </BrowserRouter>
     )
