24ad81f1504ff34c95b6c1b5efc3211e1312a7ef
Tampoka <goal.thanks@gmail.com>
2021-11-20 11:01:28 +0900

    Add navbar + style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Posts from "./pages/Posts";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import About from "./pages/About";
+import "./styles/App.css"
 
 const App = () => {
     return (
         <BrowserRouter>
+            <div className="navbar">
+                <div className="navbar__links">
+                    <Link to="/about">About</Link>
+                    <Link to="/posts">Posts</Link>
+                </div>
+            </div>
             <Routes>
                 <Route path="/about" element={<About/>}>
-            </Route>
+                </Route>
                 <Route path="/posts" element={<Posts/>}>
                 </Route>
             </Routes>
