ee22b023e6e209b866d0670e510808cf7b21c2e1
Tampoka <goal.thanks@gmail.com>
2021-11-20 16:55:32 +0900

    Create AppRouter component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import Posts from "./pages/Posts";
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import About from "./pages/About";
+import {BrowserRouter} from "react-router-dom";
 import "./styles/App.css"
 import Navbar from "./components/UI/Navbar/Navbar";
-import Error from "./pages/Error";
+import AppRouter from "./components/AppRouter";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Navbar/>
-            <Routes>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/posts" element={<Posts/>}/>
-                <Route path="/error" element={<Error/>}/>
-                <Route path="*" element={<Navigate to="/error"/>}/>
-            </Routes>
+            <AppRouter/>
         </BrowserRouter>
     )
         ;
