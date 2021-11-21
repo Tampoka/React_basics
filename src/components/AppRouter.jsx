02e3615c55dfc1aebdd3a9a3c232f9a0ed59b17d
Tampoka <goal.thanks@gmail.com>
2021-11-21 12:58:36 +0900

    Add conditional rendering on isAuth

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
 import {privateRoutes, publicRoutes} from "../router/routes";
+import {AuthContext} from "../context";
 
 const AppRouter = () => {
-    const isAuth=true
+    const {isAuth} = useContext(AuthContext)
+    console.log(isAuth)
     return (
         isAuth
-        ?
-        <Routes>
-            {privateRoutes.map(route =>
-                <Route path={route.path} element={route.component} exact={route.exact}/>
-            )}
-            <Route path="*" element={<Navigate to="/posts"/>}/>
-        </Routes>
+            ?
+            <Routes>
+                {privateRoutes.map(route =>
+                    <Route path={route.path} element={route.component} exact={route.exact} key={route.path}/>
+                )}
+                <Route path="*" element={<Navigate to="/posts"/>}/>
+            </Routes>
             :
             <Routes>
                 {publicRoutes.map(route =>
-                    <Route path={route.path} element={route.component} exact={route.exact}/>
+                    <Route path={route.path} element={route.component} exact={route.exact} key={route.path}/>
                 )}
                 <Route path="*" element={<Navigate to="/login"/>}/>
             </Routes>
