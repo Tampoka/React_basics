5049c0e6e51492e3a335b9e435f7f6fa22ed176f
Tampoka <goal.thanks@gmail.com>
2021-11-21 12:57:47 +0900

    Add AuthContext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter} from "react-router-dom";
 import "./styles/App.css"
 import Navbar from "./components/UI/Navbar/Navbar";
 import AppRouter from "./components/AppRouter";
+import {AuthContext} from "./context";
 
 const App = () => {
+    const [isAuth, setIsAuth]=useState(false)
     return (
-        <BrowserRouter>
+        <AuthContext.Provider value={{isAuth,setIsAuth}}>
+            <BrowserRouter>
             <Navbar/>
             <AppRouter/>
         </BrowserRouter>
+        </AuthContext.Provider>
     )
         ;
 };
