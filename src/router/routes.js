6595f23854e45e9957794465c527a222c3fdfeae
Tampoka <goal.thanks@gmail.com>
2021-11-21 12:03:50 +0900

    Add mapping routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,8 +3,8 @@ import Posts from "../pages/Posts";
 import PostIdPage from "../pages/PostIdPage";
 
 export const routes = [
-    {path: '/about', component: About, exact: true},
-    {path: '/posts', component: Posts, exact: true},
-    {path: '/posts/:id', component: PostIdPage, exact: true},
+    {path: '/about', component: <About/>, exact: true},
+    {path: '/posts', component: <Posts/>, exact: true},
+    {path: '/posts/:id', component: <PostIdPage/>, exact: true},
     // {path: '/error', component: Error, exact: false},
 ]
